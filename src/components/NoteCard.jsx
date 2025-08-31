53f3aa3d1662a2374511a4cc4dab115152d4d827
RehanIslam09 <rehanislamcontact@gmail.com>
2025-08-31 13:19:04 +0530

    Adding fakeData.js + improving styles

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteCard = ({ note }) => {
+    const body = JSON.parse(note.body);
+    return (
+        <div>{body}</div>
+    );
+};
+export default NoteCard
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { fakeData as notes } from '../assets/fakeData.js'
+import NoteCard from '../components/NoteCard.jsx'
 
 const NotesPage = () => {
   return (
-    <div>NotesPage</div>
-  )
+    <div>
+      {notes.map(note => (
+        <NoteCard key={note.$id} note={note} />
+      ))}
+    </div>
+  );
 }
 
 export default NotesPage
