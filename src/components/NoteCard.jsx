48982391300c71ac769c8bab1adbc150a56b25f8
RehanIslam09 <rehanislamcontact@gmail.com>
2025-08-31 14:31:22 +0530

    finalizing the card styles

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { useRef, useEffect } from 'react';
 import Trash from '../assets/icons/Trash';
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
   const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  const autoGrow = (textAreaRef) => {
+    const { current } = textAreaRef;
+    current.style.height = 'auto'; // Reset the height
+    current.style.height = current.scrollHeight + 'px'; // Set the new height
+  };
+
   return (
     <div
       className="card"
@@ -22,8 +34,10 @@ const NoteCard = ({ note }) => {
       </div>
       <div className="card-body">
         <textarea
+          ref={textAreaRef}
           style={{ color: colors.colorText }}
           defaultValue={body}
+          onInput={() => autoGrow(textAreaRef)}
         ></textarea>
       </div>
     </div>
