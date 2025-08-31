b746aea0504867a6ad6839528b523fe8f286c152
RehanIslam09 <rehanislamcontact@gmail.com>
2025-08-31 14:19:30 +0530

    adding trash icon, fixing positioning

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Trash from '../assets/icons/Trash';
 
 const NoteCard = ({ note }) => {
-    const body = JSON.parse(note.body);
-    return (
-        <div>{body}</div>
-    );
+  const body = JSON.parse(note.body);
+  const position = JSON.parse(note.position);
+  const colors = JSON.parse(note.colors);
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+        ></textarea>
+      </div>
+    </div>
+  );
 };
-export default NoteCard
+export default NoteCard;
