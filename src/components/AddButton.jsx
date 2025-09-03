1c17a3548fcfff7f0680bd6fdbb9b6dfa73c3255
RehanIslam09 <rehanislamcontact@gmail.com>
2025-09-03 15:57:26 +0530

    adding notes

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import Plus from '../assets/icons/Plus.jsx';
+import colors from '../assets/colors.json';
+import { useRef } from 'react';
+import { db } from '../appwrite/databases.js';
+import { useContext } from 'react';
+import { NoteContext } from '../context/NoteContext.jsx';
 
 const AddButton = () => {
+  const { setNotes } = useContext(NoteContext);
+  const startingPos = useRef(100);
+
+  const addNote = async () => {
+    const payload = {
+      position: JSON.stringify({
+        x: startingPos.current,
+        y: startingPos.current,
+      }),
+      colors: JSON.stringify(colors[0]),
+    };
+    startingPos.current += 10;
+
+    const response = await db.notes.create(payload);
+    setNotes((prevState) => [response, ...prevState]);
+  };
+
   return (
-    <div id="add-btn">
+    <div id="add-btn" onClick={addNote}>
       <Plus />
     </div>
   );
