5d29554bad78cb3e5539eb596ad68c2bb4308fad
RehanIslam09 <rehanislamcontact@gmail.com>
2025-09-03 15:28:21 +0530

    adding the add button and making it visually appealing

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Plus from '../assets/icons/Plus.jsx';
+
+const AddButton = () => {
+  return (
+    <div id="add-btn">
+      <Plus />
+    </div>
+  );
+};
+
+export default AddButton;
